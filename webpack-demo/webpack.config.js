50367fab0bfd4ec70404bc450638c6e1b0c8f4c9
AntonioHincapie <marcohincapiem@gmail.com>
2022-05-04 12:19:28 -0500

    Experiment with webpack

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}
